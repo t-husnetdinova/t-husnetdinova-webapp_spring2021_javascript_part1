d08ab0254991d8a20cef292224321f6a4b61466d
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-02-15 20:48:49 -0700

    factorize function added and working as expected

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// function factorize that returns the factors of an integer
+// every number that is a factor is pushed into an array
 function factorize(n) {
     var factors = [];
     for(var i = 0; i <= n; i++) {
